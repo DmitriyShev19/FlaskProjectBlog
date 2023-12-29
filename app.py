dc3de68bab51ddc28242364f6a3792412b9a5e21
dimka2338 <liamsmit444@gmail.com>
2023-12-29 22:14:22 +0300

    Добавил изменения во front

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'jsdfhjsdhkjfk4212hsdfhlkjs'
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html', title='Домашняя страница')
 
 
 if __name__ == '__main__':
